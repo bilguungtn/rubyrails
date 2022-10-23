16f5cb659275d17da29c1c68c052d683527322e7
bilguungtn <b.baasansuren@gtn.co.jp>
2022-10-23 21:32:29 +0800

    crud app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,16 +14,37 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    puts @article
     if @article.save
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
   end
-  private
-    def article_params
-      puts article_params
-      params.require(:article).permit(:title, :body)
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+  
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
